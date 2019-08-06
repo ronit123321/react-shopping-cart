25ef6ac75c6b79a580eceaf9541c2ed9bc4b5acf
ronit123321 <ronit.oommen@gmail.com>
2019-08-07 01:01:10 +0530

    implemented bootstrap

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,15 +4,37 @@ import Shelf from '../Shelf';
 import Filter from '../Shelf/Filter';
 import GithubCorner from '../github/Corner';
 import FloatCart from '../FloatCart';
+import SideRack from '../SideRack';
+
+import {Bootstrap, Grid, Row, Col} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
+
+import './style.scss';
 
 const App = () => (
   <React.Fragment>
-    <GithubCorner />
-    <main>
-      <Filter />
-      <Shelf />
-    </main>
-    <FloatCart />
+
+  <main>
+    <nav>
+      <SideRack />
+    </nav>
+
+    <header>
+      <h1>Header Title</h1>
+    </header>
+
+    <section>
+      <FloatCart />
+    </section>
+
+    <section>
+        <Filter />
+        <Shelf />
+    </section>
+
+  </main>
   </React.Fragment>
 );
 
---
+++ src/components/SideRack/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class SideRack extends Component {
+
+
+    render() {
+        return (
+        <div className='side-rack'>
+            <h1>SideRack</h1>
+        </div>
+        )}
+
+}
+
+export default SideRack;
